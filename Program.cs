5811a6d2625dd0105fa5fffe7be757462501642c
leanne-basin <leanne.basin@gmail.com>
2025-03-19 17:47:20 +0100

    version ajout par l'utilisateur

--- Program.cs
+++ Program.cs
@@ -4,7 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        var result = ConvertMinutesToHoursMinutes(190);
+        Console.WriteLine("Veuillez entrer un nombre de minutes :");
+        int nbMin = int.Parse(Console.ReadLine());
+
+        var result = ConvertMinutesToHoursMinutes(nbMin);
         Console.WriteLine($"{result.Item1}:{result.Item2}");
     }
 
