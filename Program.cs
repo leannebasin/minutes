bc9bb94c7877dfdb7f43f55a37b4507d864aca86
leanne-basin <leanne.basin@gmail.com>
2025-03-19 17:27:42 +0100

    création du programme

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var result = ConvertMinutesToHoursMinutes(190);
+        Console.WriteLine($"{result.Item1}:{result.Item2}");
+    }
+
+    static (int, int) ConvertMinutesToHoursMinutes(int nbMin)
+    {
+        int hours = nbMin / 60;
+        int minutes = nbMin % 60;
+        return (hours, minutes);
+    }
+}
